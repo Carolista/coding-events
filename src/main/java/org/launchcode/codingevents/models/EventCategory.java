d738497a7ad57fb62d9ee2a0d1588dcac4a93298
Carolista <55961845+Carolista@users.noreply.github.com>
2020-01-25 15:57:48 -0600

    chap 18.3 done

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,6 +3,8 @@ package org.launchcode.codingevents.models;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity {
@@ -10,6 +12,9 @@ public class EventCategory extends AbstractEntity {
     @NotBlank(message = "Name of category is required")
     @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory() {}
 
@@ -25,4 +30,7 @@ public class EventCategory extends AbstractEntity {
         this.name = name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 }
